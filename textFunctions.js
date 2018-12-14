ee456d24aed64852689c2b8b0f4e81ebc6741d72
Aman <aman.mender@tegningtil3d.no>
2018-12-14 15:37:01 -0800

    Cleaned up last line add, and added it to the same loop

--- textFunctions.js
+++ textFunctions.js
@@ -37,71 +37,26 @@ const formatTextsByColumnWithSpace = function(string,columnLength) {
 	var lastIndex = null;
 
     for(var i = 0; i < string.length; i++) {
-        // Check current line 
-        let startPosition = lastIndex ? lastIndex : (i+1)-columnLength;
-        let endPosition = lastIndex ? (lastIndex+columnLength) :(i+1);
         if(!((i+1) % columnLength)) {
+                // Check current line 
+                let startPosition = lastIndex ? lastIndex : (i+1)-columnLength;
+                let endPosition = lastIndex ? (lastIndex+columnLength) :(i+1);
 
-				//console.log("startPosition ",startPosition,endPosition);				
                 var currentLine = helperFunctions.createCurrentline(string,startPosition,endPosition);
-                //console.log(currentLine);
 				var lastIndexSpace = currentLine.lastIndexOf(" ");
 				lastIndex = lastIndexSpace+startPosition;
 				var l = helperFunctions.createCurrentline(currentLine,0,lastIndexSpace);
-
-                //console.log(l);
-                
+ 
                 formattedString += (helperFunctions.addNewLine(l.trim()));
                 
                 // Last line add 
                 if(string.length - i < columnLength) {
-                    console.log("Adding line ",i,l);
+                    formattedString += string.substring(i+1).trim();
                 }
 
-        } else if((string.length-i) === (columnLength-1)) { //(string.length - i) < columnLength
-            /*
-                console.log("Last string",i,((string.length-i)-columnLength));
-                //console.log(string.substring((i+1)-columnLength));
-                var _newString = string.substring((i+1)-columnLength);
-                console.log(_newString,_newString.length);
-                console.log("String length ",string.length," Current index ",i," ColumnLength ",columnLength);
-                //console.log(string.length - i);
-                //console.log(i,string[i],columnLength, ((i+1) % columnLength));
-            
-           let fullRemainderString = string.substring((i+1)-columnLength);
-           let currentLine = fullRemainderString.substring(0,columnLength);
-           let lastIndex = currentLine.lastIndexOf(' ');
-           //console.log("lastIndex ",lastIndex);
-           let secondLastString = currentLine.substring(0,lastIndex);
-           console.log("fullRemainderString ",fullRemainderString);
-           console.log("secondLastString",secondLastString);
-           console.log(formattedString);
-           //formattedString += ('\n'+secondLastString+'\n').trim();
-           // Split last line
-           //formattedString += fullRemainderString.substring(lastIndex);
-
-           /*
-           let currentLine = helperFunctions.createCurrentline(string,startPosition,endPosition);
-           let lastIndexSpace = currentLine.lastIndexOf(" ");
-           let lastIndex = lastIndexSpace+startPosition;
-           let l = helperFunctions.createCurrentline(currentLine,0,lastIndexSpace);
-           let almostLastString = (helperFunctions.addNewLine(l.trim()));
-           //console.log('almostLastString ',almostLastString);
-           console.log(lastIndex);
-           let remainderString = string.substring(lastIndex);
-           formattedString += remainderString;
-           */
-            //console.log('remainderString ',remainderString);
-            
         }
-        /*
-        console.log("Line! ",i);
-        if(!(i+1 < string.length)) {
-            console.log("Last!")
-        }
-        */
     }
-    //console.log(formattedString)
+
     return formattedString
 }
 
