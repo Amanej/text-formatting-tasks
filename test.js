6a7343223eb7077b5eef2a0292d899e5a6be6c36
Aman <aman.mender@tegningtil3d.no>
2018-12-15 15:31:26 -0800

    Refactored text functions into single object

--- test.js
+++ test.js
@@ -1,40 +1,73 @@
 const assert = require('assert');
-const textFunctions  = require('./textFunctions.js');
+const textFunctions = require('./textFunctions.js');
 
-const loremIpsumString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 const newsPaperParagraph = 'Du har kanskje sett reklame for dem rundtom på nettet der folk tar dem på seg og blir «mindblown» av lyden. Nuraphone er definitivt ikke som andre hodetelefoner, med særdeles fiffig teknologi innabords, bass ut av en annen verden (om du vil) og et veldig spesiell design.For to år siden samlet skaperne inn over 15 millioner kroner på folkefinanseringstjenesten Kickstarter, og med det var Nuraphone Australias mest innbringende Kickstarter-produkt noensinne.Året senere, altså i 2017, var de i salg i enkelte land, og først nå i høst har de kommet i offisielt salg her i Norge.';
+const texts = [{string: loremIpsum, name: 'Lorem Ipsum'},{string: newsPaperParagraph, name: 'Newspaper paragraph 1'}];
+const maxWidths = [20,30,40];
 
-
-describe('Test text formatting functions', function() {
-    const maxWidths = [10,20,30,40];
+describe('Test text formatting functions', () => {
     describe('Max width new line', function() {
-        // Test Max Width of New Line
-            maxWidths.forEach(w => {
-                console.log('Width ',w);
-                var formatTextTo10 = textFunctions.formatTextToColumns(loremIpsumString,w);
-                it('Check that no lines is longer than '+w, function() {
-                    var maxLetter10 = formatTextTo10.split('\n').filter(e => e.length > w);
-                    assert.equal(maxLetter10.length, 0)
+        texts.forEach(t => {
+            describe('Testing width '+t.name, () => {
+                maxWidths.forEach(w => {
+                    let formattedText = textFunctions.formatText.toColumns(t.string,w);
+                    it('Check that no lines is longer than '+w, function() {
+                        let maxLetters = formattedText.split('\n').filter(e => e.length > w);
+                        assert.equal(maxLetters.length, 0)
+                    });
+                });           
+            })
+        });
+    });
+    describe('Function: New line after space', () => {
+        texts.forEach(t => {
+            describe('Testing width '+t.name, () => {
+                maxWidths.forEach(w => {
+                    let formattedText = textFunctions.formatText.withSpace(t.string,w);
+                    it('Check that no lines is longer than '+w, function() {
+                        let maxLetters = formattedText.split('\n').filter(e => e.length > w);
+                        assert.equal(maxLetters.length, 0)
+                    });
+                    // Line never start or end with a space
+                    it('Whitespace checks', () => {
+                        let unTrimmedLines = formattedText.split('\n').filter(e => {
+                            return (e.endsWith(' ') || e.startsWith(' '));
+                        });
+                        assert.equal(unTrimmedLines,false);
+                        // Line should never contain just whitespace
+                        let linesWithNoSpace = formattedText.split('\n').filter(e => {
+                            return false
+                        });
+                        assert.equal(linesWithNoSpace.length,0);
+                    });
                 });
-            });           
-    });
-    describe('New line after space', function() {
-        maxWidths.forEach(w => {
-            var formattedText = textFunctions.formatTextsByColumnWithSpace(newsPaperParagraph,w);
-            it('Check that no lines is longer than '+w, function() {
-                var maxLetters = formattedText.split('\n').filter(e => e.length > w);
-                assert.equal(maxLetters.length, 0)
             });
         });
     });
-    describe('Add hyphen function', function() {
-        maxWidths.forEach(w => {
-            var formattedText = textFunctions.formatTextWithHyphen(newsPaperParagraph,w);
-            it('Check that no lines is longer than '+w, function() {
-                var maxLetters = formattedText.split('\n').filter(e => e.length > w);
-                assert.equal(maxLetters.length, 0)
+    describe('Add hyphen function', () => {
+        texts.forEach(t => {
+            describe('Testing width '+t.name, () => {
+                maxWidths.forEach(w => {
+                    let formattedText = textFunctions.formatText.withHyphen(t.string,w);
+                    it('Check that no lines is longer than '+w, function() {
+                        let maxLetters = formattedText.split('\n').filter(e => e.length > w);
+                        assert.equal(maxLetters.length, 0)
+                    });
+                    // Line should never contain just whitespace
+                    it('Whitespace checks', () => {
+                        let unTrimmedLines = formattedText.split('\n').filter(e => {
+                            return (e.endsWith(' ') || e.startsWith(' '));
+                        });
+                        assert.equal(unTrimmedLines,false);
+                        // Line should never contain just whitespace
+                        let linesWithNoSpace = formattedText.split('\n').filter(e => {
+                            return false
+                        });
+                        assert.equal(linesWithNoSpace.length,0);
+                    });
+                });
             });
         });
-
     });
 });
--- textFunctions.js
+++ textFunctions.js
@@ -1,24 +1,5 @@
-// Format lines to n characters
-const formatTextToColumns = function(string,columnLength) {
-    var formattedString = "";
-    for(var i = 0; i < string.length; i++) {
-        formattedString += string[i];
-        if(!((i+1) % columnLength)) {
-            formattedString += "\n";
-        }
-    }
-    return formattedString
-}
-
-// Split on space
-// Format lines to n characters
-    /// Conditions
-    /// Split space if possible
-    /// Shorter than width - close as possible to n characters length
-    /// Never end with space
-    /// Never just whitespace
-const formatTextsByColumnWithSpace = function(string,columnLength) {
-    const helperFunctions = {
+const formatText = {
+    helperFunctions: {
         // Check each line has no additional characters behind
         lineEndsWithSpace:(line) => {
             return line.endsWith(' ');
@@ -38,95 +19,87 @@ const formatTextsByColumnWithSpace = function(string,columnLength) {
             };            
         },
         generateLineAndIndex:(string,position) => {
-            const currentLine = helperFunctions.createCurrentline(string,position.start,position.end);
+            const currentLine = formatText.helperFunctions.createCurrentline(string,position.start,position.end);
             const lastIndexSpace = currentLine.lastIndexOf(" ");
             const lastIndex =  lastIndexSpace+position.start;
             return {
-                line: helperFunctions.createCurrentline(currentLine,0,lastIndexSpace),
+                line: formatText.helperFunctions.createCurrentline(currentLine,0,lastIndexSpace),
                 lastIndex: lastIndex
             }
         }
-    };
-
-    var formattedString = "";
-	var lastIndex = null;
-
-    for(var i = 0; i < string.length; i++) {
-        const iCalculable = (i+1);
-        const lastCharInColumn = !((iCalculable) % columnLength);
-        if(lastCharInColumn) {
-                // Check current line 
-                let position = helperFunctions.createPosition(lastIndex,iCalculable,columnLength);
-                const lineAndIndex = helperFunctions.generateLineAndIndex(string,position);
-                lastIndex = lineAndIndex.lastIndex;
- 
-                formattedString += (helperFunctions.addNewLine(lineAndIndex.line.trim()));
-                
-                // Last line add 
-                if(string.length - i < columnLength) {
-                    formattedString += string.substring(iCalculable).trim();
-                }
-
+    },
+    // Format lines to n characters
+    toColumns:(string,columnLength) => {
+        var formattedString = "";
+        for(var i = 0; i < string.length; i++) {
+            formattedString += string[i];
+            if(!((i+1) % columnLength)) {
+                formattedString += "\n";
+            }
         }
+        return formattedString
+    },
+    // Split on space
+    withSpace:(string,columnLength) => {
+        var formattedString = "";
+        var lastIndex = null;
+        for(var i = 0; i < string.length; i++) {
+            const iCalculable = (i+1);
+            const lastCharInColumn = !((iCalculable) % columnLength);
+            if(lastCharInColumn) {
+                    // Check current line 
+                    let position = formatText.helperFunctions.createPosition(lastIndex,iCalculable,columnLength);
+                    const lineAndIndex = formatText.helperFunctions.generateLineAndIndex(string,position);
+                    lastIndex = lineAndIndex.lastIndex;
+     
+                    formattedString += (formatText.helperFunctions.addNewLine(lineAndIndex.line.trim()));
+                    
+                    // Last line add 
+                    if(string.length - i < columnLength) {
+                        formattedString += string.substring(iCalculable).trim();
+                    }
+    
+            }
+        }
+        return formattedString
+    },
+    // Split with hyphen
+    withHyphen:(string,columnLength) => {
+        var formattedString = "";
+        var lastIndex = null;
+    
+        for(var i = 0; i < string.length; i++) {
+            if(!((i+1) % columnLength)) {
+                    let line = '';
+                    // Check current line 
+                    let position = {
+                        start: lastIndex ? lastIndex : (i+1)-columnLength,
+                        end: lastIndex ? (lastIndex+columnLength) :(i+1)
+                    }
+                    let currentLine = formatText.helperFunctions.createCurrentline(string,position.start,position.end);
+                    let checkLine = formatText.helperFunctions.createCurrentline(string,position.start,position.end+1);
+    
+                    let lastIndexSpace = checkLine.lastIndexOf(" ");
+                    if(lastIndexSpace) {
+                        lastIndex = lastIndexSpace+position.start;
+                        line = formatText.helperFunctions.createCurrentline(currentLine,0,lastIndexSpace);
+                    } else {
+                        lastIndex = 10+position.start;
+                        line = currentLine+'-';
+                    }
+     
+                    formattedString += (formatText.helperFunctions.addNewLine(line.trim()));
+                    
+                    // Last line add 
+                    if(string.length - i < columnLength) {
+                        formattedString += string.substring(i+1).trim();
+                    }
+    
+            }
+        }
+    
+        return formattedString    
     }
-
-    return formattedString
 }
 
-// Split with hyphen
-const formatTextWithHyphen = function(string,columnLength) {
-    const helperFunctions = {
-        // Check each line has no additional characters behind
-        lineEndsWithSpace:(line) => {
-            return line.endsWith(' ');
-        },
-        // Adds newline
-        addNewLine:(string) => {
-            return string += "\n";
-        },
-        // Create currentline
-        createCurrentline:(string,startPosition,endPosition) => {
-            return currentLine = string.substring(startPosition,endPosition);
-        },
-        checkWordLength:(string, maxLength) => {
-            return string <= maxLength;
-        }
-    };
-
-    var formattedString = "";
-	var lastIndex = null;
-
-    for(var i = 0; i < string.length; i++) {
-        if(!((i+1) % columnLength)) {
-                let line = '';
-                // Check current line 
-                let position = {
-                    start: lastIndex ? lastIndex : (i+1)-columnLength,
-                    end: lastIndex ? (lastIndex+columnLength) :(i+1)
-                }
-                let currentLine = helperFunctions.createCurrentline(string,position.start,position.end);
-                let checkLine = helperFunctions.createCurrentline(string,position.start,position.end+1);
-
-                let lastIndexSpace = checkLine.lastIndexOf(" ");
-                if(lastIndexSpace) {
-                    lastIndex = lastIndexSpace+position.start;
-                    line = helperFunctions.createCurrentline(currentLine,0,lastIndexSpace);
-                } else {
-                    lastIndex = 10+position.start;
-                    line = currentLine+'-';
-                }
- 
-                formattedString += (helperFunctions.addNewLine(line.trim()));
-                
-                // Last line add 
-                if(string.length - i < columnLength) {
-                    formattedString += string.substring(i+1).trim();
-                }
-
-        }
-    }
-
-    return formattedString    
-};
-
-module.exports = {formatTextToColumns,formatTextsByColumnWithSpace,formatTextWithHyphen};
+module.exports = {formatText};
